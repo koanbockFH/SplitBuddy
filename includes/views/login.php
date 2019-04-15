13fdd27aea50ed26eed68cefb8c2934c3a74f94b
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2019-04-15 16:50:17 +0200

    login design fertig #29

--- includes/views/login.php
+++ includes/views/login.php
@@ -3,28 +3,43 @@
 echo $this->header;
 
 ?>
-<div class="loginForm">
-    <h1>Anmelden</h1>
 
-        <div>
-            <label for="user">Benutzername oder E-Mail</label>
-            <input type="text" class="form-control" id="user"  name="user">
-            <label id="userFeedback" class="feedback"></label>
+
+    <div class="container">
+        <!-- loginfelder -->
+        <div class="row">
+            <div class="col-sm-2">
+
+            </div>
+
+            <div class="col-sm-8">
+                <div class="loginForm">
+                    <h1>Anmelden</h1>
+
+                    <div>
+                        <label for="user">Benutzername oder E-Mail</label>
+                        <input type="text" class="form-control" id="user"  name="user">
+                        <label id="userFeedback" class="feedback"></label>
+                    </div>
+
+                    <div>
+                        <label for="password">Passwort</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                        <label id="passwordFeedback" class="feedback"></label>
+                    </div>
+
+                    <p>Noch kein Konto? Jetzt <a href="register">HIER</a> registrieren</p>
+
+                    <button type="submit" class="btn" id="submitPassword" value="Absenden">Anmelden</button>
+                </div>
+            </div>
+
+
+            <div class="col-sm-2">
+
+            </div>
         </div>
-
-        <div>
-            <label for="password">Passwort</label>
-            <input type="password" class="form-control" id="password" name="password">
-            <label id="passwordFeedback" class="feedback"></label>
-        </div>
-
-        <p>Noch kein Konto? Jetzt <a href="register">HIER</a> registrieren</p>
-
-        <button type="submit" class="btn" id="submitPassword" value="Absenden">Anmelden</button>
-</div>
-
-
-
+    </div>
 
 
 
