947fbd228f441af9d3c14c62b8bcc92b1c016808
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2019-04-18 19:31:16 +0200

    ergebnis umbau #25

--- js/ergebnis.js
+++ js/ergebnis.js
@@ -4,6 +4,8 @@ function ergebnis() {
     var vname = document.getElementById(vorname);
     var nname = document.getElementById(nachname);
 
+
+
     document.getElementById(vorname).value = vname;
     document.getElementById(nachname).value = nname;
 
