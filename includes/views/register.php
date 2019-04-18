c3a871882b394ea5e0430c4c22f49a73ff35bba1
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2019-04-18 19:46:52 +0200

    register form fertig #8

--- includes/views/register.php
+++ includes/views/register.php
@@ -6,18 +6,65 @@ echo $this->header;
 
     <h1>Registrieren</h1>
     <form class="needs-validation" novalidate id="login-form">
+
         <div class="form-row justify-content-center">
+
             <div class="col-5 col-md-4 mb-3 ">
                 <label for="regFirstname" class="d-none d-md-block">Vorname*</label>
                 <input type="text" class="form-control" id="regFirstname" placeholder="Vorname" required>
                 <label id="feedbackFirstname" class="feedback"></label>
             </div>
+
             <div class="col-5 col-md-4 mb-3">
                 <label for="regLastname" class="d-none d-md-block">Nachname*</label>
                 <input type="text" class="form-control" id="regLastname" placeholder="Nachname" required>
                 <label id="feedbackFirstname" class="feedback"></label>
             </div>
+
         </div>
+
+        <div class="form-row justify-content-center">
+
+            <div class="col-5 col-md-4 mb-3 ">
+                <label for="regUser" class="d-none d-md-block">Benutzername*</label>
+                <input type="text" class="form-control" id="regFirstname" placeholder="Benutzername" required>
+                <label id="feedbackUser" class="feedback"></label>
+            </div>
+
+            <div class="col-5 col-md-4 mb-3">
+                <label for="regMail" class="d-none d-md-block">E-Mail*</label>
+                <input type="text" class="form-control" id="regMail" placeholder="E-Mail" required>
+                <label id="feedbackMail" class="feedback"></label>
+            </div>
+
+        </div>
+
+        <div class="form-row justify-content-center">
+
+            <div class="col-5 col-md-4 mb-3 ">
+                <label for="regPassword" class="d-none d-md-block">Passwort*</label>
+                <input type="text" class="form-control" id="regPassword" placeholder="Passwort" required>
+                <label id="feedbackPassword" class="feedback"></label>
+
+                <div class="passwordStrength" id="passwordWrapper">
+
+                    <div class="error feedback"></div>
+                    <div class="weak feedback">schwaches Passwort</div>
+                    <div class="moderate feedback">mittleres Passwort</div>
+                    <div class="strong feedback">starkes Passwort</div>
+
+
+                </div>
+            </div>
+
+            <div class="col-5 col-md-4 mb-3">
+                <label for="regPasswordControl" class="d-none d-md-block">E-Mail*</label>
+                <input type="text" class="form-control" id="regPasswordControl" placeholder="Passwort überprüfen" required>
+                <label id="feedbackPasswordControl" class="feedback"></label>
+            </div>
+
+        </div>
+
 
         <button type="submit" class="btn" id="submitPassword" value="Absenden">Jetzt Registrieren</button>
     </form>
