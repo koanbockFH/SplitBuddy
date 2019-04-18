ff51daa92df42d1c2d9f5626ec72eb14c12b3ebc
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2019-04-18 12:10:32 +0200

    ergebnis probe-commit #25

--- includes/views/ergebnis.php
+++ includes/views/ergebnis.php
@@ -6,27 +6,18 @@ echo $this->header;
 
 <h3>Ergebnis</h3>
 
-<?php
 
-echo $this->gruppe;
-
-?>
-
-<?php
-
-echo $this->gruppe;
-
-?>
 
 <div class="container">
     <div class="row">
         <div class="col-sm-6">
-            <h3>Column 1</h3>
-            <p>Lorem ipsum dolor..</p>
-            <p>Ut enim ad..</p>
+            <h3 id="group">g</h3>
+            <span id="vorname">v</span>
+            <span id="nachname">n</span>
+
         </div>
         <div class="col-sm-6">
-            <h3>Column 2</h3>
+            <h3 id="group">Column 2</h3>
             <p>Lorem ipsum dolor..</p>
             <p>Ut enim ad..</p>
         </div>
---
+++ js/ergebnis.js
@@ -0,0 +1,23 @@
+function ergebnis() {
+
+    //namen einlesen
+    var vname = document.getElementById(vorname);
+    var nname = document.getElementById(nachname);
+
+    document.getElementById(vorname).value = vname;
+    document.getElementById(nachname).value = nname;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
