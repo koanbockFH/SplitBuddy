9e181613e5ac89e05a9fd312b49415700e37fa99
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2019-04-22 11:01:45 +0200

    ergebnis design fertig, beispiel 4 Gruppen #25

--- includes/views/ergebnis.php
+++ includes/views/ergebnis.php
@@ -4,25 +4,80 @@ echo $this->header;
 
 ?>
 
-<h3>Ergebnis</h3>
+<h1>Ergebnis</h1>
 
 
 
 <div class="container">
+
     <div class="row">
-        <div class="col-sm-6">
-            <h3 id="group">g</h3>
-            <span id="vorname">v</span>
-            <span id="nachname">n</span>
+        <div class="col-4 gruppe">
+            <h3 id="group">gruppenname</h3>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
 
         </div>
-        <div class="col-sm-6">
-            <h3 id="group">Column 2</h3>
-            <p>Lorem ipsum dolor..</p>
-            <p>Ut enim ad..</p>
+
+        <div class="col-4 gruppe">
+            <h3 id="group">gruppenname</h3>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
         </div>
 
     </div>
+
+
+</div>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-4 gruppe">
+            <h3 id="group">gruppenname</h3>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+        </div>
+
+        <div class="col-4 gruppe">
+            <h3 id="group">gruppenname</h3>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+            <span id="vorname">vorname</span>
+            <span id="nachname">nachname</span><br>
+
+        </div>
+
+    </div>
+
+
 </div>
 
 
