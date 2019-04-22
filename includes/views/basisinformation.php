dfca8f8483a98cd7a72a515974ca1dfa16d584de
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2019-04-22 14:23:53 +0200

    implemented requesed review changes #34

--- includes/views/basisinformation.php
+++ includes/views/basisinformation.php
@@ -4,73 +4,71 @@ echo $this->header;
 
 ?>
 
-<form class="needs-validation" novalidate id="sb-basisinformation-form">
 
-    <div class="container">
+    <h1>Basisinformationen</h1>
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 mb-3">
-                <h1>Basisinformationen</h1>
+
+    <form class="needs-validation" novalidate id="sb-basisinformation-form">
+
+        <div class="container">
+
+            <div class="form-row justify-content-center">
+                <div class="col-md-6 mb-3">
+                    <label for="title" class="d-none d-lg-block">Titel*</label>
+                    <input type="text" class="form-control" id="title" placeholder="Titel" required>
+                    <div class="invalid-feedback">Bitte geben Sie einen Titel ein </div>
+                </div>
             </div>
-        </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 mb-3">
-                <label for="title" class="d-none d-lg-block">Titel*</label>
-                <input type="text" class="form-control" id="title" placeholder="Titel" required>
-                <div class="invalid-feedback">Bitte geben Sie einen Titel ein </div>
+            <div class="form-row justify-content-center">
+                <div class="col-md-6 mb-3">
+                    <label for="comment"  class="d-none d-lg-block">Anmerkungen</label>
+                    <textarea class="form-control" id="comment" rows="4" placeholder="Anmerkungen"></textarea>
+                    <small id="note" class="form-text text-muted">*gekennzeichnete Felder sind erforderlich</small>
+                </div>
             </div>
-        </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 mb-3">
-                <label for="comment"  class="d-none d-lg-block">Anmerkungen</label>
-                <textarea class="form-control" id="comment" rows="4" placeholder="Anmerkungen"></textarea>
-                <small id="note" class="form-text text-muted">*gekennzeichnete Felder sind erforderlich</small>
+            <div class="form-row justify-content-center">
+                <div class="col-md-6 mb-3">
+                    <h3>Voraussetzungen:</h3>
+                </div>
             </div>
-        </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 mb-3">
-                <h3>Voraussetzungen:</h3>
+            <div class="form-row justify-content-center">
+                <div class="col-md-6 mb-3">
+                    <p>Um erfolgreich Gruppen bilden zu können, muss es mindestens vier Teilnehmer geben</p>
+                </div>
             </div>
-        </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 mb-3">
-                <p>Um erfolgreich Gruppen bilden zu können, muss es mindestens vier Teilnehmer geben</p>
+            <div class="button form-row justify-content-center">
+                <div class="col-md-6 mb-3">
+                    <button type="submit" class="btn btn-primary">weiter</button>
+                </div>
             </div>
+
         </div>
+    </form>
 
-        <div class="button row justify-content-center">
-            <div class="col-md-6 mb-3">
-                <button type="submit" class="btn btn-primary">weiter</button>
-            </div>
-        </div>
-
-    </div>
-</form>
-
-<script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
-</script>
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
 
 <?php
 
--- includes/views/index.php
+++ includes/views/index.php
@@ -1,8 +1,91 @@
+
 <?php
 
 echo $this->header;
 
 ?>
+
+<form class="needs-validation" novalidate id="sb-einstellungen-form">
+
+        <h1>Einstellungen</h1>
+
+        <fieldset class="form-group">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <legend class="col-form-label col-md-3 sb-subheading">Gruppeneinstellung:</legend>
+                <div class="col-md-6">
+                    <div class="custom-control custom-radio">
+                        <input type="radio" id="RadioAmountGroup" name="RadioGruppenEinstellung" class="custom-control-input" onclick="addZaehler(0)" >
+                        <label class="custom-control-label" for="RadioAmountGroup">Anzahl der Gesamtgruppen</label>
+                    </div>
+
+                    <div class="custom-control custom-radio">
+                        <input type="radio" id="RadioAmountParticipants" name="RadioGruppenEinstellung" class="custom-control-input" onclick="addZaehler(0)">
+                        <label class="custom-control-label" for="RadioAmountParticipants">Anzahl der Teilnehmer</label>
+                    </div>
+
+                    <div class="custom-control custom-radio">
+                        <input type="radio" id="RadioIndvGroup" name="RadioGruppenEinstellung" class="custom-control-input" onclick="addZaehler(1)" checked>
+                        <label class="custom-control-label" for="RadioIndvGroup">Individuelle Gruppe</label>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+
+        <fieldset class="form-group amountField" id="showAmount">
+            <div class="row" >
+                <div class="col-md-3"></div>
+                <legend class="col-form-label col-md-3 sb-subheading">Anzahl:</legend>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="number" id="amount" class="form-control" onclick="validateAmount()" onkeyup="validateAmount()">
+                        <label id="feedbackAmount" class="feedback"></label>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+
+        <fieldset class="form-group">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <legend class="col-form-label col-md-3 pt-0 sb-subheading">Sortierkriterien:</legend>
+                <div class="col-md-6">
+                    <div class="custom-control custom-radio">
+                        <input type="radio" id="RadioAge" name="RadioSortierKriterien" class="custom-control-input">
+                        <label class="custom-control-label" for="RadioAge">nach Alter</label>
+                    </div>
+
+                    <div class="custom-control custom-radio">
+                        <input type="radio" id="RadioGender" name="RadioSortierKriterien" class="custom-control-input">
+                        <label class="custom-control-label" for="RadioGender">nach Geschelcht</label>
+                    </div>
+
+                    <div class="custom-control custom-radio">
+                        <input type="radio" id="RadioNoOpt" name="RadioSortierKriterien" class="custom-control-input" checked>
+                        <label class="custom-control-label" for="RadioNoOpt">keine zusätzliche Gruppierung</label>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+
+        <div class="form-group row">
+            <div class="col-md-3"></div>
+            <div class="col-md-3 sb-subheading">Zusatzeinstellung:</div>
+            <div class="col-md-6">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="blacklisting">
+                    <label class="custom-control-label" for="blacklisting">Blacklisting</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="button row justify-content-center">
+            <div class="col-md-6 mb-3">
+                <button type="submit" class="btn btn-primary" id="submitButton">weiter</button>
+            </div>
+        </div>
+    </div>
+</form>
 
 
 <?php
