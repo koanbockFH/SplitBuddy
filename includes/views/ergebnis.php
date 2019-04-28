e2c344698a8478c6268c705bc520bd5dc8e612c5
Axel Basset <koanbockFH@users.noreply.github.com>
2019-04-28 22:46:57 +0200

    Merge Anpassungen

--- includes/views/ergebnis.php
+++ includes/views/ergebnis.php
@@ -3,48 +3,48 @@
 echo $this->header;
 
 ?>
-<h1>Ergebnis</h1>
-<div class="container">
-    <div class="row">
-        <div class="col-4 gruppe">
-            <h3 id="group">gruppenname</h3>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
+<div class="container mb-5 mt-5">
+    <h1>Ergebnis</h1>
+    <div class="container">
+        <div class="row gruppen-row">
+            <div class="col-5 gruppe">
+                <h3 id="group">gruppenname</h3>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+            </div>
+            <div class="col-5 gruppe">
+                <h3 id="group">gruppenname</h3>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+            </div>
         </div>
-        <div class="col-4 gruppe">
-            <h3 id="group">gruppenname</h3>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-4 gruppe">
-            <h3 id="group">gruppenname</h3>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-        </div>
-        <div class="col-4 gruppe">
-            <h3 id="group">gruppenname</h3>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
-            <span id="vorname">vorname</span>
-            <span id="nachname">nachname</span><br>
+        <div class="row gruppen-row">
+            <div class="col-5 gruppe">
+                <h3 id="group">gruppenname</h3>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+            </div>
+            <div class="col-5 gruppe">
+                <h3 id="group">gruppenname</h3>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+                <span id="vorname">vorname</span>
+                <span id="nachname">nachname</span><br>
+            </div>
         </div>
     </div>
 </div>
