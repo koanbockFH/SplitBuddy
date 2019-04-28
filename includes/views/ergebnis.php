d92d031f397a768c0f891221343eb59ba89c6298
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2019-04-28 11:47:01 +0200

    ergebnis request #37

--- includes/views/ergebnis.php
+++ includes/views/ergebnis.php
@@ -3,89 +3,51 @@
 echo $this->header;
 
 ?>
-
 <h1>Ergebnis</h1>
-
-
-
 <div class="container">
-
     <div class="row">
         <div class="col-4 gruppe">
             <h3 id="group">gruppenname</h3>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
         </div>
-
         <div class="col-4 gruppe">
             <h3 id="group">gruppenname</h3>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
         </div>
-
     </div>
-
-
 </div>
-
 <div class="container">
-
     <div class="row">
         <div class="col-4 gruppe">
             <h3 id="group">gruppenname</h3>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
         </div>
-
         <div class="col-4 gruppe">
             <h3 id="group">gruppenname</h3>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
             <span id="vorname">vorname</span>
             <span id="nachname">nachname</span><br>
-
         </div>
-
     </div>
-
-
 </div>
-
-
-
-
-
-
-
 <?php
 
 echo $this->footer;
