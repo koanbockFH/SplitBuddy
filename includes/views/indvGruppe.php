3ab49968cc1cb66760623b948e18f26c031ecbea
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2019-04-28 10:48:50 +0200

    minor changes in JS #39

--- includes/views/indvGruppe.php
+++ includes/views/indvGruppe.php
@@ -25,24 +25,30 @@
         </div>
         <div class="col-md-3 mb-3">
             <label for="amountIndivGroup" class="d-none d-lg-block">Anzahl der Teilnehmer:</label>
-            <input type="number" class="form-control" id="amountIndivGroup" placeholder="Anzahl der Teilnehmer" required>
-            <div class="invalid-feedback">Bitte geben Sie eine Anzahl ein!</div>
+            <input type="number" class="form-control" id="amountIndivGroup" placeholder="Anzahl der Teilnehmer"  onkeyup="checkForPositiveNumbers()" onclick="checkForPositiveNumbers()" required>
+            <div class="invalid-feedback" id="amountIndivGroupFeedback">Bitte geben Sie eine Anzahl ein!</div>
         </div>
+    </div>
 
-        <div class="form-row justify-content-center">
-            <div class="col-md-12 mb-3" id="add">
-                <button class="btn btn-primary sb-add-group" type="submit">Hinzufügen</button>
-            </div>
-            <div class="col-md-12 mb-3 d-none" id="change">
-                <button class="btn btn-primary sb-add-group" type="submit">Ändern </button>
-            </div>
-
+    <div class="form-row justify-content-center">
+        <div class="col-md-12 mb-3" id="add">
+            <button class="btn btn-primary col-md-8 mb-3 mr-auto ml-auto d-block d-lg-none sb-add-indivGroup" type="submit">Hinzufügen
+            </button>
+            <button class="btn btn-primary col-md-4 mb-3 mr-auto ml-auto d-none d-lg-block sb-add-indivGroup" type="submit">Hinzufügen
+            </button>
         </div>
-
-        <div class="col-md-12 mb-3 d-none">
-            <div class="row justify-content-center"><button class="btn btn-primary" id="nextGroup" disabled>zum Ergebnis</button></div>
+        <div class="col-md-12 mb-3 d-none" id="change">
+            <button class="btn btn-primary col-md-8 mb-3 mr-auto ml-auto d-block d-lg-none sb-add-indivGroup" type="submit">Ändern
+            </button>
+            <button class="btn btn-primary col-md-4 mb-3 mr-auto ml-auto d-none d-lg-block sb-add-indivGroup" type="submit">Ändern
+            </button>
         </div>
+    </div>
+
 </form>
-<div class="col-md-12 mb-3">
-    <div class="row justify-content-center"><button class="btn btn-primary" id="nextGroup" disabled>Zum Ergebnis</button></div>
+
+<div class="row justify-content-center">
+    <button class="btn btn-primary" id="indivGroup-submit">zum Ergebnis</button>
+    <label class="text-muted text-center w-100 mt-1">mind. 2 Gruppen</label>
+    <label class="invalid-feedback text-center w-100" id="indivGroupFeedback">Vorraussetzung nicht erfüllt!</label>
 </div>
--- js/indvGruppe.js
+++ js/indvGruppe.js
@@ -1,5 +1,7 @@
+//Save all Groups to be sent to the webService
 let indivGroupList = [];
 
+//initialize form on document ready
 (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -12,7 +14,7 @@ let indivGroupList = [];
                     addGroup("groupName", "amountIndivGroup");
                 }
                 else{
-                    editGroup($("#change").attr("data-id"),"groupName", "amountIndivGroup");
+                    editGroup($("#change").attr("data-id"), "groupName", "amountIndivGroup");
                 }
                 form.reset();
                 form.classList.remove('was-validated');
@@ -25,94 +27,128 @@ let indivGroupList = [];
     }, false);
 })();
 
+//this method adds an error if the filled in number is negative
+function checkForPositiveNumbers()
+{
+    var amountId = document.getElementById("amountIndivGroup");
+    var feedbackAmountId = document.getElementById("amountIndivGroupFeedback");
+
+    var x = amountId.value;
+
+    //removes error after a failed attempt when the input is correct
+    amountId.classList.remove("is-invalid");
+    amountId.classList.add("is-valid");
+    feedbackAmountId.textContent = null;
+
+    if (x < 1)
+    {
+        amountId.classList.add("is-invalid");
+        amountId.classList.remove("is-valid");
+        feedbackAmountId.textContent = "Bitte geben Sie eine Zahl größer Null ein";
+    }
+}
+
 function addGroup(gruppennameId, anzahlId) {
     let groupNameControl = document.getElementById(gruppennameId);
     let amountControl = document.getElementById(anzahlId);
 
+    //table will be visible
     let listControl = $("#sb-indivGroup-list");
     listControl.removeClass('d-none');
 
     let currentId = indivGroupList.length;
 
-    //create editbutton
-    var editbutton = $('<button class="btn btn-secondary btn-info col-sm sb-indivGroup-list" data-id="'+ currentId +'"><i class="fas fa-pencil-alt"></i></button>');
-    editbutton.click(function(){
+    //create edit button
+    var editbutton = $('<button class="btn btn-secondary btn-info col-sm sb-icon-btn" data-id="' + currentId + '"><i class="fas fa-pencil-alt"></i></button>');
+
+    //this event will be fired when the editbutton is clicked
+    //it allows the user to change the values of his input
+    editbutton.click(function () {
         $("#change").removeClass("d-none");
         $("#add").addClass("d-none");
 
         $("#change").attr("data-id", this.dataset.id);
         loadGroup(this.dataset.id, gruppennameId, anzahlId);
     });
-    var deletebutton = $('<button class="btn btn-secondary btn-danger col-sm sb-indivGroup-btn" data-id="'+ currentId +'"><i class="fas fa-trash"></i></button>');
-    deletebutton.click(function(){
+
+    //create delete button
+    var deletebutton = $('<button class="btn btn-secondary btn-danger col-sm sb-icon-btn" data-id="' + currentId + '"><i class="fas fa-trash"></i></button>');
+
+    //this event will be fired when the delete button is clicked
+    //it allows the user to delete an entered group again
+    deletebutton.click(function () {
         deleteGroup(this.dataset.id);
     });
 
-    //Create last Cell of Row and add the Button
-    var editCell = $("<td class='sb-indivGroup-btn'></td>");
-    var deleteCell = $("<td class='sb-indivGroup-btn'></td>");
+    //Create last cell of row and add buttons
+    var editCell = $("<td class='sb-icon-btn'></td>");
+    var deleteCell = $("<td class='sb-icon-btn'></td>");
     editCell.append(editbutton);
     deleteCell.append(deletebutton);
 
     //create row
-    var row = $('<tr data-id="'+ currentId +'">');
+    var row = $('<tr data-id="' + currentId + '">');
     row.append($('<td>' + groupNameControl.value + '</td>'))
         .append($('<td>' + amountControl.value + '</td>'))
         .append($(editCell))
         .append($(deleteCell));
 
+    //adds row to the table
     listControl.append(row);
 
+    //saves values in list
     indivGroupList.push({
         groupName: groupNameControl.value,
         amount: amountControl.value,
     });
-
-    if(indivGroupList.length >= 2)
-    {
-        $('#nextGroup').attr('disabled', false);
-    }
-    else
-    {
-        $('#nextGroup').attr('disabled', true);
-    }
-
 }
 
+//this method loads the data of the group which the user wants to edit
 function loadGroup(id, groupNameId, amountId) {
     let groupNameControl = document.getElementById(groupNameId);
     let amountControl = document.getElementById(amountId);
 
-    //Get Data from
+    //get data from the list
     let item = indivGroupList[id];
 
     groupNameControl.value = item.groupName;
     amountControl.value = item.amount;
 }
 
-function editGroup(id, groupNameId, amountId){
+
+function editGroup(id, groupNameId, amountId) {
     let groupNameControl = document.getElementById(groupNameId);
     let amountControl = document.getElementById(amountId);
 
     let row = $('tr[data-id='+ id +']');
 
+    //The find() method returns descendant elements of the selected element
     let cells = row.find("td");
+
+    //the content of the cells will be changed to the new entered values
     cells[0].textContent = groupNameControl.value;
     cells[1].textContent = amountControl.value;
 
+    //removes the old value in the array
+    /*Parameters:
+    index = specifies the position of the element which will be removed
+    howmany = the number of items to be removed
+     */
     indivGroupList.splice(id,1);
 
+    //adds the new values to the array
     indivGroupList.push({
         groupName: groupNameControl.value,
         amount: amountControl.value,
     });
 
+    //changes the visibility of the buttons
     $("#add").removeClass("d-none");
     $("#change").addClass("d-none");
 }
 
 function deleteGroup(id){
-    //ask if he wants to delete
+    //asks for confirmation if the group should be deleted
     if(!confirm("Löschen?")){
         return;
     }
@@ -122,21 +158,25 @@ function deleteGroup(id){
     row.remove();
     indivGroupList.splice(id,1);
 
+    //if there are no values in the list the table won't be visible anymore
     if(indivGroupList.length === 0)
     {
         $("#sb-indivGroup-list").addClass("d-none");
     }
+}
 
-    if(teilnehmerDaten.length >= 2)
+function validateGroup()
+{
+    let result = false;
+    if(teilnehmerDaten.length >=2)
     {
-        $('#nextGroup').attr('disabled', false);
+        $("#indivGroupFeedback").removeClass('d-block');
+        result = true;
     }
-
-    else
-    {
-        $('#nextGroup').attr('disabled', true);
+    else{
+        $("#indivGroupFeedback").addClass('d-block');
     }
-
+    return result;
 }
 
     
