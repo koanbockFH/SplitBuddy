528cfd7e0e515fe7b22ed6f3898197c5c929972c
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2019-05-01 18:59:28 +0200

    adapted requested changes in register #61

--- includes/views/register.php
+++ includes/views/register.php
@@ -8,31 +8,31 @@ echo $this->header;
         <h1 class="display-1">Registrieren</h1>
             <form action="register" method="post" novalidate id="register-form">
                 <div class="form-row justify-content-center">
-                    <div class="col-10 col-md-4 mb-md-3 mb-2">
+                    <div class="col-10 col-md-5 mb-md-3 mb-2">
                         <label for="regFirstname" class="d-none d-md-block">Vorname</label>
                         <input type="text" class="form-control" id="regFirstname" placeholder="Vorname" required name="regFirstname">
                         <div id="feedbackFirstname" class="feedback"></div>
                     </div>
-                    <div class="col-10 col-md-4 mb-md-3 mb-2">
+                    <div class="col-10 col-md-5 mb-md-3 mb-2">
                         <label for="regLastname" class="d-none d-md-block">Nachname</label>
                         <input type="text" class="form-control" id="regLastname" placeholder="Nachname" required name="regLastname">
                         <div id="feedbackLastname" class="feedback"></div>
                     </div>
                 </div>
                 <div class="form-row justify-content-center">
-                    <div class="col-10 col-md-4 mb-md-3 mb-2">
+                    <div class="col-10 col-md-5 mb-md-3 mb-2">
                         <label for="regUser" class="d-none d-md-block">Benutzername</label>
                         <input type="text" class="form-control" id="regUser" placeholder="Benutzername" required name="regUser">
                         <div id="feedbackUser" class="feedback"></div>
                     </div>
-                    <div class="col-10 col-md-4 mb-md-3 mb-2">
+                    <div class="col-10 col-md-5 mb-md-3 mb-2">
                         <label for="regMail" class="d-none d-md-block">E-Mail</label>
                         <input type="email" class="form-control" id="regMail" placeholder="E-Mail" required name="regMail">
                         <div id="feedbackMail" class="feedback"></div>
                     </div>
                 </div>
                 <div class="form-row justify-content-center">
-                    <div class="col-10 col-md-4 mb-md-3 mb-2">
+                    <div class="col-10 col-md-5 mb-md-3 mb-2">
                         <label for="regPassword" class="d-none d-md-block">Passwort</label>
                         <input type="password" class="form-control" id="regPassword" placeholder="Passwort" required name="regPassword">
                         <div class="passwordStrength error" id="passwordWrapper">
@@ -44,7 +44,7 @@ echo $this->header;
                             <div class="strong">starkes Passwort</div>
                         </div>
                     </div>
-                    <div class="col-10 col-md-4 mb-md-3 mb-2">
+                    <div class="col-10 col-md-5 mb-md-3 mb-2">
                         <label for="regPasswordControl" class="d-none d-md-block">Passwort überprüfen</label>
                         <input type="password" class="form-control" id="regPasswordControl" placeholder="Passwort überprüfen" required name="regPasswordControl">
                         <div id="feedbackPasswordControl" class="feedback"></div>
